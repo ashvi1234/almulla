4e08bd6385ba9859ee9e95bb6b2c0a08e66e12e4
Ashwini Apurkar <ashviapurkar1094@gmail.com>
2020-10-06 13:26:12 +0530

    Initial Commit

---
+++ AlmullaDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlmullaDemoApp
+//
+//  Created by Ashwini Apurkar on 06/10/20.
+//  Copyright © 2020 Ashwini Apurkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
